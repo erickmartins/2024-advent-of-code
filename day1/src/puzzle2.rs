d216a4f379774b1633546cfbd5541d1011a5a4af
Erick Martins Ratamero <erickmartins@gmx.net>
2024-12-01 12:50:39 -0500

    day 1 part 2 done

--- day1/src/puzzle2.rs
+++ day1/src/puzzle2.rs
@@ -4,6 +4,23 @@ use std::io;
 pub fn puzzle2() -> io::Result<usize> {
     let mut similarity = 0;
 
+    let input = fs::read_to_string("puzzle1/input.txt")?;
+    let input_lines = input.lines();
+    let mut first: Vec<usize>= vec![];
+    let mut second: Vec<usize>= vec![];
+    let line_array:Vec<String> = input_lines.map(String::from).collect();
+    for line in line_array{
+        let mut parts = line.splitn(2, "   ");
+        first.push(parts.next().unwrap().parse().unwrap());
+        second.push(parts.next().unwrap().parse().unwrap());
+    }
+
+    for i in 0..first.len(){
+        let val = first[i];
+        let factor = second.iter().filter(|&n| *n == val).count();
+        similarity = similarity + (val * factor);
+    }
+
     return Ok(similarity);
 }
 
