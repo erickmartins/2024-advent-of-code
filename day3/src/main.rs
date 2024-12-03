6859d42b1b2970fb1964897a3ec2b9e9bc815711
Erick Martins Ratamero <erickmartins@gmx.net>
2024-12-03 12:30:17 -0500

    day 3 done

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -6,17 +6,46 @@ use regex::Regex;
 fn main() {
     let mults = puzzle1().unwrap();
     println!("solution part 1: {mults}");
+    let mults_en = puzzle2().unwrap();
+    println!("solution part 2: {mults_en}");
 }
 
-fn puzzle1() -> io::Result<usize> {
-    let mut mults = 0;
-    // let input = fs::read_to_string("input.txt")?; 
-    // let input_str = input.as_str();   
-    // let re = Regex::new(r"mul\([0-9]{1,3},[0-9]{1,3}\)").unwrap();
-    let re = Regex::new(r"([0-9]{4})-([0-9]{2})-([0-9]{2})").unwrap();
-    let hay = "On 2010-03-14, I became a Tenneessee lamb.";
-    let (_, [year, month, day]) =re.captures(hay).map(|caps| caps.extract()).unwrap();
-    // let (full, [first, second]) = re.captures(input_str).map(|caps| caps.extract()).unwrap();
-    dbg!(year);
+fn puzzle1() -> io::Result<i32> {
+    let mut mults:i32 = 0;
+    let input = fs::read_to_string("input.txt")?; 
+    let input_str = input.as_str();   
+    let re = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
+    for (_, [first, second]) in re.captures_iter(input_str).map(|caps| caps.extract()){
+        let first_val = first.parse::<i32>().unwrap();
+        let second_val = second.parse::<i32>().unwrap();
+        mults = mults + (first_val * second_val);
+    }
+    
+    return Ok(mults);
+}
+
+fn puzzle2() -> io::Result<i32> {
+    let mut mults:i32 = 0;
+    let input = fs::read_to_string("input.txt")?; 
+    let input_str = input.as_str();
+    let re = Regex::new(r"(mul\([0-9]{1,3},[0-9]{1,3}\))|(do\(\))|(don\'t\(\))").unwrap();
+    let sub_re = Regex::new(r"([0-9]{1,3}),([0-9]{1,3})").unwrap();
+    let mut on = true;
+
+    for (_, [ext_str]) in re.captures_iter(input_str).map(|caps| caps.extract()){
+        dbg!(ext_str);
+        if ext_str == "do()" {
+            on = true;
+        }else if ext_str == "don't()"{
+            on = false;
+        }else if on{
+            let caps = sub_re.captures(ext_str).unwrap();
+            let first_val = &caps[1].parse::<i32>().unwrap();
+            let second_val = &caps[2].parse::<i32>().unwrap();
+            mults = mults + (first_val * second_val);
+        }
+    }
+
+    
     return Ok(mults);
 }
